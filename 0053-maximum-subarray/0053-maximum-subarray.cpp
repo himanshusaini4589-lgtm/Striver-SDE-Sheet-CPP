f6c19d003181d05a5766f24b3464acb0a828f7bc
himanshusaini4589-lgtm <himanshusaini4589@gmail.com>
2026-06-30 18:56:12 +0530

    Time: 0 ms (100%), Space: 74.5 MB (14.52%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,13 +4,13 @@ public:
         int n = nums.size();
         if (n == 1) return nums[0];
 
-        vector<int> dp(n + 1);
+        vector<int> dp(n);
 
-        dp[1] = nums[0];          
-        int maxi = dp[1];
+        dp[0] = nums[0];          
+        int maxi = dp[0];
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1]);
+        for (int i = 1; i < n; i++) {
+            dp[i] = max(nums[i], dp[i - 1] + nums[i]);
             maxi = max(maxi, dp[i]);
         }
         return maxi;
